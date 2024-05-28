1daebe1335ba8177327ecfac1a283339dcb0cf04
Billy Dunn <bdunn27@gmail.com>
2024-05-28 17:38:18 -0400

    sitter profile

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -10,7 +10,7 @@ function NavBar() {
       <NavLink to="/pets" className="nav-link">
         Pets
       </NavLink>
-      <NavLink to="/sitter" className="nav-link">
+      <NavLink to="/sitters/{id}" className="nav-link">
         Sitter
       </NavLink>
       <NavLink to="/owner/1" className="nav-link">
