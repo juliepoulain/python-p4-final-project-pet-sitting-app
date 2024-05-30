9ab6c313126a45acb97ac840c1f690546245a17c
Billy Dunn <bdunn27@gmail.com>
2024-05-30 18:32:22 -0400

    pet image

--- server/combined_seed.py
+++ server/combined_seed.py
@@ -23,9 +23,9 @@ with app.app_context():
     print("Deleting pets...")
     Pet.query.delete()
     print("Adding pets...")
-    garfield = Pet(name="Garfield", image= "", animal="cat", age=3, temperament="lazy", owner_id=julie.id)
-    rose = Pet(name="Rose", image= "", animal="cat", age=1, temperament="not friendly", owner_id=billy.id)
-    buddy = Pet(name="Buddy", image= "", animal="dog", age=11, temperament="friendly", owner_id=billy.id)
+    garfield = Pet(name="Garfield", image="https://images.pexels.com/photos/4587955/pexels-photo-4587955.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", animal="cat", age=3, temperament="lazy", owner_id=julie.id)
+    rose = Pet(name="Rose", image="https://img.freepik.com/free-photo/close-up-portrait-beautiful-cat_23-2149214419.jpg", animal="cat", age=1, temperament="not friendly", owner_id=billy.id)
+    buddy = Pet(name="Buddy", image="https://image.shutterstock.com/image-photo/happy-puppy-dog-smiling-on-260nw-1799966587.jpg", animal="dog", age=11, temperament="friendly", owner_id=billy.id)
     pets=[garfield, rose, buddy]
     db.session.add_all(pets)
     db.session.commit()
