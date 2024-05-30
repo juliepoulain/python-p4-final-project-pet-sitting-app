ccacd2130f1bda43a2c819c860d5d129cb5126b9
beatriz <beatrizffeo@gmail.com>
2024-05-30 14:17:24 -0600

    navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,24 +4,30 @@ import { NavLink } from "react-router-dom";
 function NavBar({ ownerId }) {
   return (
     <nav className="navbar">
-      <NavLink to="/" className="nav-link">
-        Home
-      </NavLink>
-      {ownerId ? (
-        <NavLink to={`/pets`} className="nav-link">
-          My Pets
+      <div className="nav-links-container">
+        <NavLink to="/" exact className="nav-link" activeClassName="active">
+          Home
         </NavLink>
-      ) : (
-        <NavLink to="/login" className="nav-link">
-          My Pets
+        {ownerId ? (
+          <NavLink to="/pets" className="nav-link" activeClassName="active">
+            My Pets
+          </NavLink>
+        ) : (
+          <NavLink to="/login" className="nav-link" activeClassName="active">
+            My Pets
+          </NavLink>
+        )}
+        <NavLink
+          to={`/owner/${ownerId}`}
+          className="nav-link"
+          activeClassName="active"
+        >
+          Profile
         </NavLink>
-      )}
-      <NavLink to={`/owner/${ownerId}`} className="nav-link">
-        Profile
-      </NavLink>
-      <NavLink to="/login" className="nav-link">
-        Log Out
-      </NavLink>
+        <NavLink to="/login" className="nav-link" activeClassName="active">
+          Log Out
+        </NavLink>
+      </div>
     </nav>
   );
 }
